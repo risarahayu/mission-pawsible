035d0d8532d9b60e84fe21ad4c3680352466d780
Yuda <iudasvastika@gmail.com>
2023-11-07 11:46:13 +0800

    menambahkan role ke table user dan membuat user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,24 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('users')->insert([
+            'name' => 'Admin Pertama',
+            'email' => 'admin_1@example.com',
+            'password' => '123qweasd',
+            'role' => 'admin',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'User Pertama',
+            'email' => 'user_1@example.com',
+            'password' => '123qweasd',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'User Kedua',
+            'email' => 'user_2@example.com',
+            'password' => '123qweasd',
+        ]);
     }
 }
